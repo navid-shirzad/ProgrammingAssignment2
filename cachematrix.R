0b8c73accb20c2caa6cdd6ff03e12b4d50d09136
Navid-Shirzad <navidshirzd@gmail.com>
2016-05-02 20:57:49 -0700

    Navid Shirzad's updated files, week 3 assignment, final version

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function computes the inverse of the special "matrix" returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+## matrix has not changed), then cacheSolve should retrieve the inverse from the
+## cache. Also, if det(matrix) = 0 the function returns NULL as inverse of the 
+## matrix.
 cacheSolve <- function(x, ...) {
      inv <- x$getinverse()
      if(!is.null(inv)) {
@@ -33,7 +35,16 @@ cacheSolve <- function(x, ...) {
           return(inv)
      }
      data <- x$get()
-     inv <- solve(data, ...)
+     # check determinant of the matrix, if det is zero matrix does not have an 
+     # inverse (function will return Null as inverse).
+     if (det(data) == 0){
+          print("matrix is not invertible")
+          inv <- NULL
+     }
+     else {
+          inv <- solve(data, ...)
+     }
+     #cache the inverse matrix and return its numeric value
      x$setinverse(inv)
      inv
 }
